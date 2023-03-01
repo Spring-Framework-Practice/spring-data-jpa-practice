e89776ea9bbba929e0d07c6c53b26d12562974e8
zubair basha <mohammedzubair006@gmail.com>
2023-03-01 23:08:56 +0530

    1.Added the Custom Queries in the JPA. 2.Added the Custom Queries using the Property.-M Zubair Basha

--- src/main/java/com/springdatajpa/practicespringdatajpa/Entity/Customer.java
+++ src/main/java/com/springdatajpa/practicespringdatajpa/Entity/Customer.java
@@ -14,6 +14,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Entity
 @Builder
 @Data
--- src/main/java/com/springdatajpa/practicespringdatajpa/Repository/CustomerRepository.java
+++ src/main/java/com/springdatajpa/practicespringdatajpa/Repository/CustomerRepository.java
@@ -1,12 +1,15 @@
 package com.springdatajpa.practicespringdatajpa.Repository;
-
+import com.springdatajpa.practicespringdatajpa.Entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import com.springdatajpa.practicespringdatajpa.Entity.Customer;
 @Repository
+
 public interface CustomerRepository extends JpaRepository<Customer,Long>{
-    
-    //JPA Repository for Customer
-    //added a new comment
+   //Derived Query
+   long countByLastName(String lastName);
+   List<Customer> findByLastNameOrderByFirstNameAsc(String lastName);
+   //traverse by property
+   List<Customer> findByAddressMandal(String mandal);
 }
