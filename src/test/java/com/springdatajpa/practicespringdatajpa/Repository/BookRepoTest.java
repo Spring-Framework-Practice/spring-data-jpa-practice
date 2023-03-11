15e2fe8a05f2fe113797a81a53eecf0865be10c9
zubair basha <mohammedzubair006@gmail.com>
2023-03-11 23:13:32 +0530

    Practiced Query creation,pagenation,sorting in the spring data jpa

--- src/test/java/com/springdatajpa/practicespringdatajpa/Repository/BookRepoTest.java
+++ src/test/java/com/springdatajpa/practicespringdatajpa/Repository/BookRepoTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.springdatajpa.practicespringdatajpa.Entity.Book;
 import com.springdatajpa.practicespringdatajpa.Entity.BookCategory;
@@ -39,4 +42,52 @@ public class BookRepoTest {
             System.out.println(books);
         }
     }
+    @Test
+    @DisplayName("Returns find by book name")
+    public void findByBookName(){
+        Book book=repo.findByBookName("python");
+        System.out.println(book);
+        //This will return find by book name ignoring the case sensitive
+        Book book2=repo.findByBookNameIgnoreCase("java");
+        System.out.println(book2);
+    }
+    @Test
+    public void orderBy(){
+        List<Book> books=repo.findAllByOrderByIdAsc();
+        System.out.println(books);
+    }
+    @Test
+    @DisplayName("Paging the Elements in the Book Element")
+    public void pagenationImplementation(){
+        Pageable page=PageRequest.of(1, 1);
+        List<Book> books=repo.findAll(page).getContent();
+        Long totalElement=repo.findAll(page).getTotalElements();
+        int totalPages=repo.findAll(page).getTotalPages();
+        System.out.println("Total Pages:"+totalPages);
+        System.out.println(totalElement);
+        System.out.println(books);
+
+    }
+    @Test
+    @DisplayName("Using Sort Mechanism")
+    public void sortByBook(){
+        Sort sort=Sort.by("bookName").descending();
+        List<Book> books=repo.findAll(sort);
+        System.out.println(books);
+    }
+    @Test
+    public void sortByLimit(){
+        /*To find the record based on the limit of the record.
+        We can use Top or First after the limit size. Use by after
+        defining the limit. We can use Pagenable and Sort mechanism in the 
+        limit
+        */
+        List<Book> book=repo.findFirst1ByBookName("java");
+        System.out.println(book);
+        List<Book> book2=repo.findFirst1ByBookName("python",
+        Sort.by("id").descending());
+        System.out.println(book2);
+    }
+
+
 }
