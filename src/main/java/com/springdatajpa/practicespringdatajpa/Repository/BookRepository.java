e3792c6a73f33c47922e6296866725a2e4f27da1
zubair basha <mohammedzubair006@gmail.com>
2023-03-14 21:45:47 +0530

    Practiced the customed query

--- src/main/java/com/springdatajpa/practicespringdatajpa/Repository/BookRepository.java
+++ src/main/java/com/springdatajpa/practicespringdatajpa/Repository/BookRepository.java
@@ -1,11 +1,16 @@
 package com.springdatajpa.practicespringdatajpa.Repository;
 import com.springdatajpa.practicespringdatajpa.Entity.Book;
 
+import jakarta.persistence.LockModeType;
 import jakarta.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.history.RevisionRepository;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long>{
@@ -16,4 +21,26 @@ public interface BookRepository extends JpaRepository<Book,Long>{
     List<Book> findAllByOrderByIdAsc();
     List<Book> findFirst1ByBookName(String bookName);
     List<Book> findFirst1ByBookName(String bookName,Sort sort);
+    Book findDistinctByBookName(String bookName);
+    Book findByBookNameStartingWith(String starting);
+    //Custom Queries
+    //JPQL(Java Persistance Query Language)
+    /*
+     * JPQL follows the Class not the database name. 
+     * We need to define the select statement for the class not the 
+     * Database table and Column name
+     */
+    @Query("select b from Book b where b.bookName=?1")
+    Book getBookName(String bookName);
+    @Query("select b from Book b where b.id=?1")
+    Book getBookById(Long id);
+    //Native Query
+    @Query(value="select * from book b where b.book_name=?1",
+        nativeQuery=true)
+    Book getBookname(String bookName);
+    //Native Query with Named Query
+    @Query(value="Select * from book b where b.book_name=:bookName",
+    nativeQuery = true)
+    //@Lock(LockModeType.READ)
+    Book getBookNameByParam(@Param("bookName")String bookName);
 }
